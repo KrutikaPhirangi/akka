44544f3b45c17b43c39f23ada6431f701254da5f
KrutikaPhirangi <138781661+KrutikaPhirangi@users.noreply.github.com>
2024-04-01 11:41:15 +0530

    Update HelloActor.scala

--- app/HelloActor.scala
+++ app/HelloActor.scala
@@ -1,23 +1,22 @@
 import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+import scala.concurrent.duration._
 
 class ActorA(actorB: ActorRef) extends Actor {
+  import context.dispatcher
   def receive: Receive = {
     case "startLoop" =>
       println("Starting loop in ActorA")
       context.become(looping)
-      self ! "sendMessage"
+      context.system.scheduler.scheduleOnce(5.seconds, self, "sendMessage")
   }
 
   def looping: Receive = {
     case "sendMessage" =>
       println("Sending message to actor B")
       actorB ! "Hi.. this is message from actor A"
+      context.system.scheduler.scheduleOnce(5.seconds, self, "sendMessage")
     case "Sending response from actor B to actor A" =>
       println(s"Received response from actor B")
-      self ! "sendMessage"
-    case "stopLoop" =>
-      println("Stopping loop in ActorA")
-      context.unbecome()
   }
 }
 
@@ -25,16 +24,15 @@ class ActorB extends Actor {
   def receive: Receive = {
     case msg: String =>
       println(s"Received message from actorA: $msg")
+      Thread.sleep(2000)
       sender() ! "Sending response from actor B to actor A"
   }
 }
-object ActorB {
-  def props: Props = Props[ActorB]
-}
+
 object Main extends App {
   private val system = ActorSystem("HelloSystem")
-  private val actorB = system.actorOf(ActorB.props, "actorB")
-  private val actorA = system.actorOf(Props(new ActorA(actorB)), name = "actorA")
 
+  private val actorB = system.actorOf(Props[ActorB], "actorB")
+  private val actorA = system.actorOf(Props(new ActorA(actorB)), name = "actorA")
   actorA ! "startLoop"
 }
